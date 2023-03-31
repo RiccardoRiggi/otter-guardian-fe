8dfbda8dd92172a4e61952c05c8d5644147e6643
Riccardo Riggi <ghirowinformatica@gmail.com>
2023-03-31 23:51:10 +0200

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,9 +6,6 @@ import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-import * as am4core from "@amcharts/amcharts4/core";
-import * as am4charts from "@amcharts/amcharts4/charts";
-import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
 
 export default function HomePage() {
